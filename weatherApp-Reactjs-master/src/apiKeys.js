27c54f13487b136aed4af14b9ef4a2999b9839d8
vandan033 <152582909+vandan033@users.noreply.github.com>
2025-04-22 21:11:47 +0530

    Add files via upload

---
+++ weatherApp-Reactjs-master/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "ebad76d0037ddfd72e1c537ec6adb289",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
